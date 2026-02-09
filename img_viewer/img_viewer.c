72ec28e0cc2cf0931ee5ba2ad32d685d743b8b6f
Hazet Alex <alex.hazet@gmail.com>
2026-02-09 12:05:56 +0100

    tests

--- img_viewer/img_viewer.c
+++ img_viewer/img_viewer.c
@@ -61,6 +61,7 @@ void print_only_char(FILE* p_file){
     int c;
     int in_group = 0;
     int group_count = 1;
+    int n_chunk_types = 0;
 
     while((c = getc(p_file)) != EOF) {
         if (isalpha(c)) {
@@ -68,7 +69,11 @@ void print_only_char(FILE* p_file){
                 printf(" ");
                 group_count++;
             }
-            printf("GC : %d", group_count);
+            printf("%d", c);
+            if (group_count == 4) {
+                n_chunk_types ++;
+                printf(" HERE");
+            }
             in_group = 1;
         } else {
             if (in_group) {
@@ -82,6 +87,7 @@ void print_only_char(FILE* p_file){
     if (in_group) {
         printf("\n");
     }
+    printf("chunk types : %d\n", n_chunk_types);
 }
 
 int main(int argc, char* argv[]) {
@@ -93,8 +99,8 @@ int main(int argc, char* argv[]) {
     FILE* p_file = fopen(argv[1], "r");
 
     if (p_file == NULL) {
-        printf("Failed to open the file");
-        fclose(p_file);
+        printf("Failed to open the file\n");
+        //fclose(p_file); /* What if I don't close the stream ?*/
         return 1;
     }
 
@@ -109,9 +115,9 @@ int main(int argc, char* argv[]) {
         printf("This is NOT a png file\n");
         return 1;
     }
-    //print_file(p_file);
+    print_file(p_file);
     reset_file(p_file);
-    print_only_char(p_file);
+    //print_only_char(p_file);
     printf("File size : %d\n", file_size);
 
     fclose(p_file);
