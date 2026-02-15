77a09255a1d0d6e48c29e13ffabaaa19964aa2b7
Alex <alex.hazet@gmail.com>
2026-02-15 17:00:53 +0100

    cleaned version

--- raylib_clock/raylib_clock.c
+++ raylib_clock/raylib_clock.c
@@ -6,7 +6,7 @@
 #define WIDTH  900
 #define HEIGHT 600
 #define DEG_TO_RAD (M_PI / 180.0)
-#define LINE_THICNESS 5
+#define LINE_THICNESS 5 
 
 const int CLOCK_RADIUS = HEIGHT * 0.45;
 
@@ -16,6 +16,9 @@ const float HOUR_HAND_LENGTH    = 0.5 * CLOCK_RADIUS;
 const float MINUTE_HAND_LENGTH  = 0.6 * CLOCK_RADIUS;
 const float SECONDS_HAND_LENGTH = 0.8 * CLOCK_RADIUS;
 
+// Angle progressions since it's used everywhere
+const float HOUR_AP   = 360.0/12.0;
+const float MINSEC_AP = 360.0/60.0;
 
 int clamp(int v) {
     if (v < 0) return 0;
@@ -23,7 +26,7 @@ int clamp(int v) {
     return v;
 }
 
-struct Color createColor(int r, int g, int b, int a) {
+struct Color create_color(int r, int g, int b, int a) {
     return (struct Color){
         clamp(r),
         clamp(g),
@@ -49,24 +52,25 @@ void draw_minute_markers() {
         float x_out = CLOCK_CENTER.x + CLOCK_RADIUS * 0.98 * cosf(alpha * DEG_TO_RAD);
         float y_out = CLOCK_CENTER.y + CLOCK_RADIUS * 0.98 * sinf(alpha * DEG_TO_RAD);
         Vector2 out = {x_out, y_out};
-        alpha += 360.0 / 60.0;
-        DrawLineEx(in, out, LINE_THICNESS, createColor(0, 0, 0, 255));
+        alpha += MINSEC_AP;
+        DrawLineEx(in, out, LINE_THICNESS, create_color(0, 0, 0, 255));
     }
 }
 
 
-// TODO : fix the position offset
 void draw_hour_numbers() {
-    float alpha = -90.0 + (360.0/12.0);
+
+    int   position_correction = 3;
+    float alpha = -90.0 + HOUR_AP;;
     float x, y;
+
     for (int i = 0 ; i < 12 ; i++) {
-        x = CLOCK_CENTER.x + CLOCK_RADIUS * 0.80 * cos(alpha * DEG_TO_RAD);
-        y = CLOCK_CENTER.y + CLOCK_RADIUS * 0.80 * sin(alpha * DEG_TO_RAD);
-        alpha += 360.0 / 12.0;
-        DrawRectangle(x, y, 6, 6, createColor(255, 0, 0, 128));
-        int text_size = MeasureText(TextFormat("%d", (i+1)), 30); 
-        //printf("%d : %d\n",  (i + 1), text_size);
-        DrawText(TextFormat("%d", (i+1) ), x, y, 30, createColor(0, 0, 0, 255));
+        x = CLOCK_CENTER.x + CLOCK_RADIUS * 0.75 * cos(alpha * DEG_TO_RAD);
+        y = CLOCK_CENTER.y + CLOCK_RADIUS * 0.75 * sin(alpha * DEG_TO_RAD);
+
+        alpha += HOUR_AP;
+        int text_size = MeasureText(TextFormat("%d", (i+1)), 30) / 2; 
+        DrawText(TextFormat("%d", (i+1) ), x - text_size, y - text_size - position_correction, 30, create_color(0, 0, 0, 255));
     }
 }
 
@@ -84,19 +88,15 @@ void draw_hour_hand(struct tm *tm) {
     y_out = CLOCK_CENTER.y + HOUR_HAND_LENGTH * sinf(alpha * DEG_TO_RAD);
     Vector2 out = {x_out, y_out};
 
-    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, createColor(0, 0, 0, 255));
+    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, create_color(0, 0, 0, 255));
 }
 
 void draw_minute_hand(struct tm *tm) {
-    
-    // seconds angle progression
-    float sec_ap = 360.0/60.0;
 
     float seconds_progression =  ((float)(tm->tm_sec)) / 60.0;
-    //printf("seconds progression : %f\n", seconds_progression);
 
-    float alpha = (((float) (tm->tm_min) * sec_ap) - 90.0);
-    alpha += sec_ap*seconds_progression;
+    float alpha = (((float) (tm->tm_min) * MINSEC_AP) - 90.0);
+    alpha += MINSEC_AP*seconds_progression;
 
     float x_out, y_out;
     x_out = CLOCK_CENTER.x + MINUTE_HAND_LENGTH * cosf(alpha * DEG_TO_RAD);
@@ -104,7 +104,7 @@ void draw_minute_hand(struct tm *tm) {
     Vector2 out = {x_out, y_out};
 
 
-    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, createColor(0, 0, 255, 255));
+    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, create_color(0, 0, 255, 255));
 }
 
 void draw_seconds_hand(struct tm *tm) {
@@ -116,8 +116,15 @@ void draw_seconds_hand(struct tm *tm) {
     y_out = CLOCK_CENTER.y + SECONDS_HAND_LENGTH * sinf(alpha * DEG_TO_RAD);
     Vector2 out = {x_out, y_out};
 
-    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, createColor(255, 0, 0, 255));}
+    DrawLineEx(CLOCK_CENTER, out, LINE_THICNESS, create_color(255, 0, 0, 255));
+}
 
+void debug_time(struct tm *tm) {
+    int hour    = tm->tm_hour;
+    int minute  = tm->tm_min;
+    int seconds = tm->tm_sec;
+    printf("Hour : %d\nminute : %d\nseconds : %d\n",hour, minute, seconds);
+}
 
 int main(void)
 {
@@ -126,20 +133,12 @@ int main(void)
     // Anti aliazing
     SetConfigFlags(FLAG_MSAA_4X_HINT);
 
-
-    Color background = createColor(0, 0, 0, 255);
-    Color clock_color = createColor(255, 255 ,255 , 255);
+    Color background  = create_color(0, 0, 0, 255);
+    Color clock_color = create_color(255, 255 ,255 , 255);
 
     time_t current_time = time(NULL);
     char* current_time_str = ctime(&current_time);
     struct tm *tm_struct = localtime(&current_time);
-    size_t size_tm = sizeof(*tm_struct);
-    printf("size of tm : %zu\n", size_tm);
-    int hour = tm_struct->tm_hour;
-    int minute = tm_struct->tm_min;
-    int seconds = tm_struct->tm_sec;
-
-    //printf("Hour : %d\nminute : %d\nseconds : %d\n",hour, minute, seconds);
 
     InitWindow(WIDTH, HEIGHT, "RayLib Basic clock");
 
